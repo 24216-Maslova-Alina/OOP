9772e818d4af375acf0fc02b1c46f85675a2442e
24216-Maslova-Alina <24216-Maslova-Alina@users.noreply.github.com>
2025-10-30 12:27:06 +0000

    Deploying to gh-pages from @ 24216-Maslova-Alina/OOP@27a4b5c5bb04ad1da2cb49320241f201ef95b876 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.a.maslova1.hashtable","l":"HashTable"},{"p":"ru.nsu.a.maslova1.hashtable","l":"Node"}];updateSearchResults();
